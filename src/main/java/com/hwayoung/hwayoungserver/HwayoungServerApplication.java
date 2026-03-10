e1d3bcd584393ad4045dd3d8a9440497913c8906
hyojun6 <yhyojun66@gmail.com>
2026-03-10 12:07:29 +0900

    INIT :: init

---
+++ src/main/java/com/hwayoung/hwayoungserver/HwayoungServerApplication.java
@@ -0,0 +1,13 @@
+package com.hwayoung.hwayoungserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwayoungServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwayoungServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwayoung/hwayoungserver/HwayoungServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwayoung.hwayoungserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwayoungServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
