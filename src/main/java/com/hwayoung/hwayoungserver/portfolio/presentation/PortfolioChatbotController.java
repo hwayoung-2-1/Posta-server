4e5e118889eab83dee800181a26e8a822410779b
Haeyul <24.007@bssm.hs.kr>
2026-05-26 09:42:31 +0900

    fix : Swagger Portfolio 챗봇 설명 추가

--- src/main/java/com/hwayoung/hwayoungserver/portfolio/presentation/PortfolioChatbotController.java
+++ src/main/java/com/hwayoung/hwayoungserver/portfolio/presentation/PortfolioChatbotController.java
@@ -7,6 +7,8 @@ import com.hwayoung.hwayoungserver.portfolio.presentation.dto.response.CreateCha
 import com.hwayoung.hwayoungserver.portfolio.presentation.dto.response.SendChatMessageResponse;
 import com.hwayoung.hwayoungserver.user.CurrentUserService;
 import com.hwayoung.hwayoungserver.user.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Tag(name = "챗봇 기능")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -26,18 +29,21 @@ public class PortfolioChatbotController {
     private final CurrentUserService currentUserService;
     private final PortfolioChatbotService portfolioChatbotService;
 
+    @Operation(summary = "ChatBot 생성 API", description = "viwer가 포토폴리오의 portfolier를 볼때 사용하는 AI 생성 API")
     @PostMapping("/portfolios/{portfolioId}/chat-sessions")
     public ResponseEntity<CreateChatSessionResponse> createSession(@PathVariable UUID portfolioId) {
         User user = currentUserService.getCurrentUser();
         return ResponseEntity.status(HttpStatus.CREATED).body(portfolioChatbotService.createSession(user, portfolioId));
     }
 
+    @Operation(summary = "AI와의 대화 세션 조회", description = "")
     @GetMapping("/chat-sessions/{chatSessionId}/messages")
     public ResponseEntity<ChatMessagesResponse> messages(@PathVariable UUID chatSessionId) {
         User user = currentUserService.getCurrentUser();
         return ResponseEntity.ok(portfolioChatbotService.messages(user, chatSessionId));
     }
 
+    @Operation(summary = "메시지 세션 생성 API")
     @PostMapping("/chat-sessions/{chatSessionId}/messages")
     public ResponseEntity<SendChatMessageResponse> sendMessage(
             @PathVariable UUID chatSessionId,
